4413c00403047c0083dc49ebc7734a09c72c3c20
zeek0x <0h23k1.n40k1@gmail.com>
2020-05-17 19:28:47 +0900

    Add 07-03.

---
+++ src/07/03.hs
@@ -0,0 +1,59 @@
+{-
+data Person = Person String String Int Float String String
+    deriving (Show)
+
+firstName :: Person -> String
+firstName (Person firstname _ _ _ _ _) = firstname
+
+lastName :: Person -> String
+lastName (Person _ lastname _ _ _ _) = lastname
+
+age :: Person -> Int
+age (Person _ _ age _ _ _) = age
+
+height :: Person -> Float
+height (Person _ _ _ height _ _) = height
+
+phoneNumber :: Person -> String
+phoneNumber (Person _ _ _ _ number _) = number
+
+flavor :: Person -> String
+flavor (Person _ _ _ _ _ flavor) = flavor
+-}
+{-
+ghci> let guy = Person "Buddy" "Finklestein" 43 184.2 "526-2928" "Chocolate"
+ghci> firstName guy
+"Buddy"
+ghci> height guy
+184.2
+ghci> flavor guy
+"Chocolate"
+-}
+
+data Person = Person { firstName :: String
+                     , lastName :: String
+                     , age :: Int
+                     , height :: Float
+                     , phoneNumber :: String
+                     , flavor :: String} deriving (Show)
+{-
+ghci> :t flavor
+flavor :: Person -> String
+ghci> :t firstName
+firstName :: Person -> String
+-}
+
+data Car = Car String String Int deriving (Show)
+{-
+ghci> Car "Ford" "Mustang" 1967
+Car "Ford" "Mustang" 1967
+-}
+
+data Car = Car { company :: String
+           , model :: String
+           , year :: Int
+           } deriving (Show)
+{-
+ghci> Car {company="Ford", model="Mustang", year=1967}
+Car {company = "Ford", model = "Mustang", year = 1967}
+-}
