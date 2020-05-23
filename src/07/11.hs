7831fd5938bee651ca00baf52777cadffc635946
zeek0x <0h23k1.n40k1@gmail.com>
2020-05-23 17:19:45 +0900

    Add 07-11.

---
+++ src/07/11.hs
@@ -0,0 +1,16 @@
+{-
+ghci> :k Int
+Int :: *
+ghci> :k Maybe
+Maybe :: * -> *
+ghci> :k Maybe Int
+Maybe Int :: *
+-}
+
+{-
+Either :: * -> * -> *
+ghci> :k Either String
+Either String :: * -> *
+ghci> :k Either String Int
+Either String Int :: *
+-}
