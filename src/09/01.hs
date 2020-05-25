25a4cb62fea47262a84331b8d5ab81ae773b6fcf
zeek0x <0h23k1.n40k1@gmail.com>
2020-05-26 01:02:26 +0900

    Add 09-01.

---
+++ src/09/01.hs
@@ -0,0 +1,44 @@
+{-
+bash-5.0$ ./capslocker < haiku.txt
+I'M A LIL' TEAPOT
+WHAT'S WITH THAT AIRPLANE FOOD, HUH?
+IT'S SO SMALL, TASTELESS
+capslocker: <stdin>: hGetLine: end of file
+-}
+
+{-
+bash-5.0$ ./capslocker < haiku.txt
+I'M A LIL' TEAPOT
+WHAT'S WITH THAT AIRPLANE FOOD, HUH?
+IT'S SO SMALL, TASTELESS
+-}
+
+{-
+bash-5.0$ ./capslocker
+hey ho
+HEY HO
+lets go
+LETS GO
+-}
+
+{-
+bash-5.0$ ./shortLinesOnly < shortlines.txt
+i'm short
+so am i
+short
+-}
+
+{-
+bash-5.0$ ./palindromes
+hehe
+not a palindrome
+ABCBA
+palindrome
+cookie
+not a palindrome
+bash-5.0$ ./palindromes < words.txt
+not a palindrome
+palindrome
+palindrome
+palindrome
+-}
