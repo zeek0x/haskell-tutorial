0798308de78bbfbc4edc72ddf8943d48db05c758
zeek0x <0h23k1.n40k1@gmail.com>
2020-05-27 23:50:46 +0900

    Add 09-03.

---
+++ src/09/03.hs
@@ -0,0 +1,25 @@
+{-
+bash-5.0$ ./appendtodo
+Iron the dishes
+bash-5.0$ ./appendtodo
+Dust the dog
+bash-5.0$ ./appendtodo
+Take salad out of the oven
+bash-5.0$ cat todo.txt
+Iron the dishes
+Dust the dog
+Take salad out of the oven
+-}
+
+{-
+bash-5.0$ ./deletetodo
+These are your TO-DO items
+0 - Iron the dishes
+1 - Dust the dog
+2 - Take salad out of the oven
+Which one do you want to delete?
+1
+bash-5.0$ cat todo.txt
+Iron the dishes
+Take salad out of the oven
+-}
