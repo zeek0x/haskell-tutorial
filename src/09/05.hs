63e666c34e6f822f31809e4ce95f5208911aa9ef
zeek0x <0h23k1.n40k1@gmail.com>
2020-06-01 00:44:11 +0900

    Add 09-05.

---
+++ src/09/05.hs
@@ -0,0 +1,17 @@
+{-
+bash-5.0$ ./todo view todo.txt
+0 - Iron the dishes
+1 - Dust the dog
+2 - Take salad out of the oven
+bash-5.0$ ./todo add todo.txt "Pick up children from dry cleaners"
+bash-5.0$ ./todo remove todo.txt 2
+These are your To-Do items:
+0 - Iron the dishes
+1 - Dust the dog
+2 - Take salad out of the oven
+3 - Pick up children from dry cleaners
+bash-5.0$ ./todo view todo.txt
+0 - Iron the dishes
+1 - Dust the dog
+2 - Pick up children from dry cleaners
+-}
